5905eb3da7195380a582f5bd114af28e6ea64ab3
hazimalik <85446767+hazimalik@users.noreply.github.com>
2025-01-26 19:52:26 +0100

    Ruletest

--- src/test/java/org/apache/commons/codec/language/bm/RuleTest.java
+++ src/test/java/org/apache/commons/codec/language/bm/RuleTest.java
@@ -48,14 +48,14 @@ public class RuleTest {
         for (final Rule.Phoneme[] phs : makePhonemes()) {
             for (int i = 0; i < phs.length; i++) {
                 for (int j = i + 1; j < phs.length; j++) {
-                    int c = Rule.Phoneme.COMPARATOR.compare(phs[i], phs[j]);
-    
-                    assertTrue(c < 0,
+                    final int c = Rule.Phoneme.COMPARATOR.compare(phs[i], phs[j]);
+
+                    assertTrue(Integer.valueOf(c).intValue() < 0,
                             "Comparing " + phs[i].getPhonemeText() + " to " + phs[j].getPhonemeText() + " should be negative");
                 }
             }
         }
-    }    
+    }
 
     @Test
     public void testPhonemeComparedToSelfIsZero() {
