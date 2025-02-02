36716f1209082c0347c4130f186eb40192ee7e4f
hazimalik <85446767+hazimalik@users.noreply.github.com>
2025-02-02 19:43:19 +0100

    name

--- src/main/java/org/apache/commons/codec/language/SoundexUtils.java
+++ src/main/java/org/apache/commons/codec/language/SoundexUtils.java
@@ -123,13 +123,6 @@ final class SoundexUtils {
     /**
      * <p>Checks if a CharSequence is empty ("") or null.</p>
      *
-     * <pre>
-     * StringUtils.isEmpty(null)      = true
-     * StringUtils.isEmpty("")        = true
-     * StringUtils.isEmpty(" ")       = false
-     * StringUtils.isEmpty("bob")     = false
-     * StringUtils.isEmpty("  bob  ") = false
-     * </pre>
      *
      * @param cs  the CharSequence to check, may be null
      * @return {@code true} if the CharSequence is empty or null
